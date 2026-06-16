cb40a0fcfffe62a5e497095e0d496ca569acfef7
Dalerick <kcadena2005@gmail.com>
2026-06-16 17:30:38 -0500

    diseño de colores

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,14 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { 
+  IonCard, 
+  IonCardContent, 
+  IonCardSubtitle, 
+  IonCardTitle, 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +16,35 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        
+        <div className="card-container">
+          <IonCard className="card">
+            <img 
+              src="https://avatars.githubusercontent.com/u/48026030?v=4" 
+              alt="Avatar" 
+            />
+            <IonHeader>
+              <IonCardTitle>Narveaz Jose</IonCardTitle>
+              <IonCardSubtitle>Narvaez Jose</IonCardSubtitle>
+              <IonCardContent>
+                <p>
+                  Desarrollador de software con experiencia en
+                  aplicaciones móviles y web. Apasionado por
+                  la tecnología.
+                </p>
+              </IonCardContent>
+            </IonHeader>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
